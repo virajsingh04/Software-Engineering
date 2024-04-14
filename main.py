b6898610d08f0a5d2e7052be4ca9f82eca75a8f9
virajsingh04 <166665584+virajsingh04@users.noreply.github.com>
2024-04-13 21:14:35 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+def encode_password(password):
+    encoded_password = ""
+    for digit in password:
+        encoded_digit = str((int(digit) + 3) % 10)
+        encoded_password += encoded_digit
+    return encoded_password
+
+
