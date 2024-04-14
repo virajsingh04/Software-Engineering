c6e003566a2196e602d507d9cd6640862b789cce
ShwinLi <liandrew0404@gmail.com>
2024-04-13 21:25:52 -0400

    Added decoder

--- main.py
+++ main.py
@@ -5,4 +5,25 @@ def encode_password(password):
         encoded_password += encoded_digit
     return encoded_password
 
+def decoder(encoded_password):
+    encoded_password = list(str(encoded_password))
+
+    for i in range(len(encoded_password)):
+        if int(encoded_password[i]) > 2:
+            encoded_password[i] = int(encoded_password[i]) - 3
+
+        elif int(encoded_password[i]) == 2:
+            encoded_password[i] = 9
+
+        elif int(encoded_password[i]) == 1:
+            encoded_password[i] = 8
+
+        elif int(encoded_password[i]) == 0:
+            encoded_password[i] = 7
+
+    for i in range(len(encoded_password)):
+        encoded_password[i] = str(encoded_password[i])
+
+    return "".join(encoded_password)
+
 
